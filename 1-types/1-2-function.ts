643713381bd5bcb02270483b3caff2bf1be2cf3d
juhyunham <judev0227@gmail.com>
2021-08-23 19:32:29 +0900

    basic typescript

---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,31 @@
+{
+	//JavaScript 💩
+	function jsAdd(num1, num2) {
+		return num1 + num2
+	}
+
+	//TypeScript ✨
+	function add(num1: number, num2: number): number {
+		return num1 + num2
+	}
+
+	//JavaScript 💩
+	function jsFetchNum(id) {
+		//code..
+		//code..
+
+		return new Promise((resolve, reject) => {
+			resolve(100);
+		})
+	}
+
+	//TypeScript ✨
+	function fetchNum(id: string): Promise<number> {
+		//code..
+		//code..
+
+		return new Promise((resolve, reject) => {
+			resolve(100);
+		})
+	}
+}
