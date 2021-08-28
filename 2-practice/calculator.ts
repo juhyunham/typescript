d5fc50e12d03c290bb0821417302683ce0650d33
juhyunham <judev0227@gmail.com>
2021-08-28 12:52:09 +0900

    calculator and node_module update

---
+++ 2-practice/calculator.ts
@@ -0,0 +1,24 @@
+
+type Operator = `add` | 'substract' | 'multiply' | 'divide' | 'remainder'
+const calculate = (operator: Operator, x: number, y: number): number => {
+	switch (operator) {
+		case 'add':
+			return x + y;
+		case 'substract':
+			return x - y;
+		case 'multiply':
+			return x * y;
+		case 'divide':
+			return x / y;
+		case 'remainder':
+			return x % y;
+		default:
+			throw Error(`unknown command`);
+	}
+}
+
+console.log(calculate(`add`, 1, 3)) //4
+console.log(calculate(`substract`, 3, 1)) //2
+console.log(calculate(`multiply`, 4, 2)) //8
+console.log(calculate(`divide`, 4, 2)) //2
+console.log(calculate(`remainder`, 5, 2)) //1
