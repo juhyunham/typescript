bf8575b5713eaf2cdab2deb7701a51b1b8563f29
juhyunham <judev0227@gmail.com>
2021-09-14 15:47:43 +0900

    stack update

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -3,3 +3,36 @@ interface Stack {
 	push(value: string): void
 	pop(): string;
 }
+
+type StackNode = {
+	readonly value: string;
+	readonly next?: StackNode
+}
+
+// 단일 연결리스트 사용하기.
+class StackImplementation implements Stack {
+	private _size: number = 0;
+	private head?: StackNode
+
+	// public
+	get size() {
+		return this._size
+	}
+
+	push(value: string) {
+		const node: StackNode = { value, next: this.head }
+		this.head = node;
+		this._size++;
+	}
+
+	pop(): string {
+		if (this.head === undefined) {
+			throw new Error(`Stack is empty`)
+		}
+
+		const node = this.head;
+		this.head = node.next;
+		this._size--;
+		return node.value;
+	}
+}
