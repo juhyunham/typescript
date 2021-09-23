74b164757306a3da239abfd2863e24b32b3bf7a1
juhyunham <judev0227@gmail.com>
2021-09-23 14:03:04 +0900

    update generic

--- 5-generic/5-3-constrains.ts
+++ 5-generic/5-3-constrains.ts
@@ -19,7 +19,7 @@ class PartTimeEmployee implements Employee {
 }
 
 // 자동으로 월급 지불! 
-// 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 소스는 똥!
+// 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 소스는 💩!
 function payBad(employee: Employee): Employee {
 	employee.pay();
 	return employee;
@@ -30,10 +30,31 @@ function pay<T extends Employee>(employeee: T): T {
 	return employeee;
 }
 
-const ham = new FullTimeEmployee();
+const hamju = new FullTimeEmployee();
 const bob = new PartTimeEmployee();
-ham.workFullTime();
+
+hamju.workFullTime();
 bob.workPartTime();
 
-const hamAfterPay = pay(ham);
+const hamjuAfterPay = pay(hamju);
 const bobAfterPay = pay(bob);
+
+const obj = {
+	name: `ham`,
+	age: 31
+}
+
+const obj2 = {
+	animal: `🐷`
+}
+
+// console.log(getValue(obj, `score`)) // score 가 없어서~ 에러뿜뿜!!
+console.log(getValue(obj, `name`)) // ham
+console.log(getValue(obj, `age`)) // 31
+console.log(getValue(obj2, `animal`)) // 🐷
+
+// keyof T : 그 obj 안에 있는 키들중에 하나여야함.
+// return : T[K] 그 obj 있는 key가 가리키고 있는 value 타입이어야한다.
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+	return obj[key];
+}
